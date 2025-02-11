229fc9990878683ad94d9b906e456a8f9d80cbc3
OpdeBrammert <bramvanotten@gmail.com>
2025-02-11 13:08:28 +0100

    commit

--- code/api.py
+++ code/api.py
@@ -137,10 +137,13 @@ class API:
         def read_app(appid: str, fuzzy: bool = True, db=self.db_dependency):
             return app_data_from_id_or_name(appid, db, fuzzy)
 
-        @self.app.post("/app_input")
-        def handle_form(game_input: str = Form(...)):
-            return {"message": game_input}
+        @self.app.post("/app_input", response_class=HTMLResponse)
+        def handle_form(request: Request, game_input: str = Form(...), db=self.db_dependency):
+            data = app_data_from_id_or_name(game_input, db, True)
 
+            return self.templates.TemplateResponse(
+                request=request, name="game_output.html", context={"games":data, "message": "Hoi"}
+            )
         @self.app.put("/app/{appid}/tag/{tagid}")
         def add_app_tag(appid: int, tagid: int, db=self.db_dependency):
             if not os.environ.get("PYCHARM_HOSTED"):
