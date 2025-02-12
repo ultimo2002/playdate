b8de864936aa2e6580fbc2fec532344a23b41285
timo <timo.rouw@outlook.com>
2025-02-12 18:56:46 +0100

    API is geen class meer

--- API_code/main.py
+++ API_code/main.py
@@ -1,8 +1,27 @@
-from api import API
+from fastapi.staticfiles import StaticFiles
+
+import api
+import config
+import uvicorn
+from fastapi import FastAPI
+
+
+#run de api
+def run():
+    api.register_endpoints(app)
+
+    print("Run API")
+
+    uvicorn.run(app, host=config.API_HOST_URL, port=config.API_HOST_PORT, reload=False)
+
 
 def main():
-    api = API()
-    api.run()
+    global app
+    # maak app FastAPI instance
+    app = FastAPI()
+    # ?? CSS ofzo?
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    run()
 
-if __name__ == "__main__":
-    main()
+
+main()
--- tests/test_api.py
+++
@@ -1,7 +0,0 @@
-from API_code import api
-from fastapi.testclient import TestClient
-client = TestClient(api.API.app)
-
-def test_run():
-    response = client.get("/")
-    assert response.status_code == 200
