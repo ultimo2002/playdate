f206f8352dbc001385da3f634fd253e62550b507
timo <timo.rouw@outlook.com>
2025-02-12 20:40:54 +0100

    debugging

--- code/test_api.py
+++ code/test_api.py
@@ -8,11 +8,11 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))  # Ensure current
 
 api_instance = api.API()
 api_instance.register_endpoints()
-
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))  # Ensure current directory is in sys.path
 client = TestClient(api_instance.app)
 
 
 
-def test_home():
+def test_():
     response = client.get("/")
     assert response.status_code == 200
