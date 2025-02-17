9df3b93f1ac77db3c210ac1e134a03bb9e292c99
SegerEnd <gj.endendijk@gmail.com>
2025-02-17 09:53:43 +0100

    More routes

--- code/api.py
+++ code/api.py
@@ -1,26 +1,21 @@
 import os
-import random
 
 from fastapi import FastAPI, Depends, Request, HTTPException
 import uvicorn
 
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
-from sqlalchemy.sql import exists
 from sqlalchemy.sql.expression import func
 
-from code.algoritmes.cache import cache_background_image, cache_header_image
 from .algoritmes.fuzzy import similarity_score, jaccard_similarity, _most_similar, make_typo
-from .config import API_HOST_URL, API_HOST_PORT, BLOCKED_CONTENT_TAGS
+from .config import API_HOST_URL, API_HOST_PORT
 
-from .routes.apps import router as apps_router
-
+from .routes.apps import router as apps_router, app_data_from_id_or_name
+from .routes.frontend import router as frontend_router
 
 import code.database.models as models
-from code.database.database import Engine, SessionLocal, get_db
-
+from code.database.database import Engine, get_db
 
 class API:
     db_dependency = None
@@ -57,45 +52,11 @@ class API:
         Function to define all the endpoints for the API.
         """
 
+        self.app.include_router(frontend_router)
+
         # register routers, only when in PYCHARM or Pytest
         if os.getenv("PYCHARM_HOSTED") or os.getenv("PYTEST_RUNNING"): # We dont wont users on production to modify the database with the CRUD endpoints.
             self.app.include_router(apps_router)
-
-        @self.app.get("/")
-        def root(request: Request, db=self.db_dependency):
-            """"
-            The root endpoint of the API when visiting the website.
-            :return: The HTML response from the index.html template.
-            """
-
-            # Get a random background_image from the database
-            background_image = (
-                db.query(models.App.background_image, models.App.id, models.App.name)
-                .filter(
-                    ~exists().where(
-                        (models.AppTags.app_id == models.App.id) &
-                        (models.AppTags.tag_id == models.Tags.id) &
-                        (models.Tags.name.in_(BLOCKED_CONTENT_TAGS))
-                    )
-                )
-                .order_by(func.random())
-                .limit(1)
-                .first()
-            )
-            try:
-                background_image = background_image if background_image else None
-            except (TypeError, IndexError, KeyError):
-                background_image = None
-
-            if os.getenv("CACHE_IMAGES") and background_image:
-                background_image = cache_background_image(background_image)
-
-
-            return self.templates.TemplateResponse(
-                request=request, name="index.html",
-                context={"message": "", "background_image": background_image}
-            )
-
 
         @self.app.get("/apps")
         def read_apps(db=self.db_dependency, all_fields: bool = False, target_name: str = None, like: str = None):
@@ -276,61 +237,6 @@ class API:
 
             return [{"name": dev.developer} for dev in developers]
 
-        @self.app.get("/recommend", response_class=HTMLResponse)
-        def handle_form(request: Request, game: str = "", db=self.db_dependency):
-            """"
-            Handle the GET request for the HTML <form> to search for a game.
-
-            :param request: The request object auto given by FastAPI.
-            :param game_input: The name or id of the game to search for. Always uses fuzzy search.
-            :return: The HTML response with the results in the context.
-            """
-            if not game:
-                return root(request, db)
-            # clean up the input to prevent XSS attacks
-            game_input = game.strip()
-            game_input = game_input.strip()
-            game_input = game_input.replace("<", "").replace(">", "")
-            del game
-
-            selected_app = app_data_from_id_or_name(game_input, db, True, True)
-
-            apps = [] # currently algorithm not implemented, Bram enable line below to start developing find_similar_games()
-            # apps = find_similar_games(selected_app, db)
-
-            if not selected_app or not selected_app.id:
-                return self.templates.TemplateResponse(
-                    request=request, name="404.html", context={"message": f"Game {game_input} not found."}
-                )
-
-
-            nsfw = False
-
-            for tag in selected_app.tags:
-                if tag.name in BLOCKED_CONTENT_TAGS:
-                    nsfw = True
-                    break
-
-            # chache the background image
-            if os.getenv("CACHE_IMAGES"):
-                if selected_app.background_image:
-                    cached = cache_background_image(selected_app)
-                    try:
-                        selected_app.background_image = cached["background_image"] if cached else selected_app.background_image
-                    except (TypeError, KeyError):
-                        selected_app.background_image = selected_app.background_image
-
-                if selected_app.header_image:
-                    cached = cache_header_image(selected_app)
-                    try:
-                        selected_app.header_image = cached["header_image"] if cached else selected_app.header_image
-                    except (TypeError, KeyError):
-                        selected_app.header_image = selected_app.header_image
-
-            return self.templates.TemplateResponse(
-                request=request, name="game_output.html", context={"selected_app":selected_app, "apps": apps,"nsfw": nsfw}
-            )
-
         def find_similar_games(selected_app, db):
             """Finds games with the most similar tags to the given game.
 
@@ -366,44 +272,6 @@ class API:
 
 
             return matching_games
-
-        def app_data_from_id_or_name(app_id_or_name: str, db, fuzzy: bool = True, categories: bool = False):
-            """"
-            Helper function to get the data for a specific app. (Not a direct endpoint)
-
-            :param app_id_or_name: The appid or name of the game to get the data for.
-            :param db: The database dependency.
-            :param fuzzy: If True, try to find the app by name even when the grammar is not correct using my fuzzy algorithm ^Seger. It skips this always when the appid is a number.
-            :param categories: If True, also get the categories, genres and tags for the app in the App object as response.
-            :return: JSON / dictionary with all the data for the app.
-            """
-            app = None
-
-            if app_id_or_name.isdigit():
-                app = db.query(models.App).filter(models.App.id == int(app_id_or_name)).first()
-            else:
-                if fuzzy:
-                    similar_app = most_similar_named_app(app_id_or_name, db)
-                    if similar_app and isinstance(similar_app.get("id"), int):
-                        print(f"Most similar app for '{app_id_or_name}' is '{similar_app['name']}' with similarity: {similar_app['similarity']}")
-                        app = db.query(models.App).filter(models.App.id == similar_app["id"]).first()
-                else:
-                    app_id_or_name = app_id_or_name.strip().capitalize()
-                    app = db.query(models.App).filter(models.App.name == app_id_or_name).first()
-                    if not app:
-                        raise HTTPException(status_code=404, detail=f"App {app_id_or_name} not found")
-
-            if categories and app and app.id:
-                tags = db.query(models.Tags).join(models.AppTags).filter(models.AppTags.app_id == app.id).all()
-                genres = db.query(models.Genre).join(models.AppGenre).filter(models.AppGenre.app_id == app.id).all()
-                categories = db.query(models.Category).join(models.AppCategory).filter(
-                    models.AppCategory.app_id == app.id).all()
-
-                app.tags = tags
-                app.genres = genres
-                app.categories = categories
-
-            return app
 
         @self.app.get("/apps/developer/{target_name}")
         def get_developer_games(target_name: str, fuzzy: bool = True, all_fields: bool = False, db=self.db_dependency):
@@ -451,22 +319,6 @@ class API:
             if most_similar_dev:
                 print(f"Most similar developer: {most_similar_dev} with similarity: {similarity}. For target: {target_name}")
                 return most_similar_dev.developer
-
-            return None
-
-        def most_similar_named_app(target_name: str, db):
-            """
-            Helper function to find the most similar named app in the database.
-
-            :param target_name: The name of the app to find the most similar named app for.
-            :param db: The database dependency.
-            :return: Dictionary / JSON with the (id, name and similarity) of the app.
-            """
-            apps = db.query(models.App).with_entities(models.App.id, models.App.name).all()
-            most_similar_app, similarity = _most_similar(target_name, apps, "name")
-
-            if most_similar_app:
-                return {"id": most_similar_app.id, "name": most_similar_app.name, "similarity": similarity}
 
             return None
 
--- code/routes/apps.py
+++ code/routes/apps.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
+from code.algoritmes.fuzzy import _most_similar
 from code.database import crud
 import code.database.models as models
 from code.database.database import get_db
@@ -29,3 +30,59 @@ def delete_app(item_id: int, db: Session = Depends(get_db)):
     if app is None:
         raise HTTPException(status_code=404, detail=f"App {app} not found")
     return {"message": "App deleted successfully"}
+
+
+def most_similar_named_app(target_name: str, db):
+    """
+    Helper function to find the most similar named app in the database.
+
+    :param target_name: The name of the app to find the most similar named app for.
+    :param db: The database dependency.
+    :return: Dictionary / JSON with the (id, name and similarity) of the app.
+    """
+    apps = db.query(models.App).with_entities(models.App.id, models.App.name).all()
+    most_similar_app, similarity = _most_similar(target_name, apps, "name")
+
+    if most_similar_app:
+        return {"id": most_similar_app.id, "name": most_similar_app.name, "similarity": similarity}
+
+    return None
+
+def app_data_from_id_or_name(app_id_or_name: str, db, fuzzy: bool = True, categories: bool = False):
+    """"
+    Helper function to get the data for a specific app. (Not a direct endpoint)
+
+    :param app_id_or_name: The appid or name of the game to get the data for.
+    :param db: The database dependency.
+    :param fuzzy: If True, try to find the app by name even when the grammar is not correct using my fuzzy algorithm ^Seger. It skips this always when the appid is a number.
+    :param categories: If True, also get the categories, genres and tags for the app in the App object as response.
+    :return: JSON / dictionary with all the data for the app.
+    """
+    app = None
+
+    if app_id_or_name.isdigit():
+        app = db.query(models.App).filter(models.App.id == int(app_id_or_name)).first()
+    else:
+        if fuzzy:
+            similar_app = most_similar_named_app(app_id_or_name, db)
+            if similar_app and isinstance(similar_app.get("id"), int):
+                print(
+                    f"Most similar app for '{app_id_or_name}' is '{similar_app['name']}' with similarity: {similar_app['similarity']}")
+                app = db.query(models.App).filter(models.App.id == similar_app["id"]).first()
+        else:
+            app_id_or_name = app_id_or_name.strip().capitalize()
+            app = db.query(models.App).filter(models.App.name == app_id_or_name).first()
+            if not app:
+                raise HTTPException(status_code=404, detail=f"App {app_id_or_name} not found")
+
+    if categories and app and app.id:
+        tags = db.query(models.Tags).join(models.AppTags).filter(models.AppTags.app_id == app.id).all()
+        genres = db.query(models.Genre).join(models.AppGenre).filter(models.AppGenre.app_id == app.id).all()
+        categories = db.query(models.Category).join(models.AppCategory).filter(
+            models.AppCategory.app_id == app.id).all()
+
+        app.tags = tags
+        app.genres = genres
+        app.categories = categories
+
+    return app
