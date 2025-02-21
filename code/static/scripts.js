4169ef26a1db317b6f11ac1fb440296e25b2c088
SegerEnd <gj.endendijk@gmail.com>
2025-02-21 11:28:47 +0100

    Voor Bilal, misschien helpt dit je starten?

---
+++ code/static/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('form[action="/recommend"]').addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let gameName = document.getElementById("game").value.trim();
+        if (gameName === "") {
+            alert("Enter a game name");
+            return;
+        }
+
+        let url = `app/similar/${encodeURIComponent(gameName)}`;
+
+        let selected_games_element = document.getElementById("selected_games");
+
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP error, status = " + response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                let new_div = document.createElement("div");
+                new_div.innerHTML = `ID: ${data.id}, Naam: ${data.name}`;
+                selected_games_element.appendChild(new_div);
+            })
+            .catch(error => {
+                console.error("Error: ", error);
+                alert("Error: " + error);
+            });
+    });
+});
